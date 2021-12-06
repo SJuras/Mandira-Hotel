63387b10c46dbcb25da541b62b016eae6dd4b6a4
SJuras <jurasstpko@gmailcom>
2021-12-06 21:58:16 +0100

    v1.1.1 initial commit, all folders, files, icons and images added.

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// toggle mobile menu
+const hambuger = document.querySelector('.hambuger');
+const navMenu = document.querySelector('.nav-menu');
+
+hambuger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hambuger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll('.nav-link');
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hambuger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+// toggle mobile menu END
+
+// change image on hero section background
+function img(anything) {
+  document.querySelector('.slide').src = anything;
+}
+
+function change(change) {
+  const line = document.querySelector('.image');
+  line.style.background = change;
+}
+// END
